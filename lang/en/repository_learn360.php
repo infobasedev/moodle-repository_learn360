bc60cb96bf772128e22a9211e12a620d6fa62784
Kenneth Scalia <kscalia@FOF2000.LOCAL>
2018-11-20 13:39:03 -0500

    adding files

---
+++ lang/en/repository_learn360.php
@@ -0,0 +1,33 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'repository_learn360', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   repository_learn360
+ * @Author    Pramod Ubbala (AGS -> Infobase)
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['configplugin'] = 'Learn360 configuration';
+$string['licensekey'] = 'Access key';
+$string['launchURl'] =  'Launch URL';
+$string['pluginname_help'] = 'Learn360';
+$string['pluginname'] = 'Learn360';
+$string['learn360:view'] = 'View the Learn360 repository';
+$string['search'] = 'Search Learn360';
+$string['customMessage'] = 'Your request cannot be validated.  Please contact Learn360 support for assistance and verify your credentials are still valid.';
